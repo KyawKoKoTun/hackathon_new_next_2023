37c6e7444d2f9bc90c19581f0bbd32b6958d9cc5
Kyaw Ko Ko Tun <kyawkokotunmm475157@gmail.com>
2023-05-21 09:55:12 +0630

    t

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ admin._menu = admin._menu[1:]
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Chair, db.session))
 
-import views.api_views
+from views.api_views import *
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
